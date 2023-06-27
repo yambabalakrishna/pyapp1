0eafcabf36217c16ae926dbc49c0a80e5122d39a
yambabalakrishna <yambabalakrishna@gmail.com>
2023-06-27 21:45:59 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from PIL import Image
 image = Image.open('nn.jpg')
-st.image(image, caption='Sunrise by the mountains')
+st.image(image, caption='it is my first web page deployment to cloud')
 st.write("Welcome")
