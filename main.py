e1698046bfcb0e5b189198a96860155ee3d5e760
yambabalakrishna <yambabalakrishna@gmail.com>
2023-06-28 07:53:32 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+st.set_page_config(layout="wide")
 image = Image.open('nn.jpg')
 st.image(image, caption='it is my first web page deployment to cloud')
 st.write("Welcome")
