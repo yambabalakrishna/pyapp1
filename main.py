8617517fcfd4558409734f7b1593c1a3b8222699
yambabalakrishna <yambabalakrishna@gmail.com>
2023-06-28 08:09:46 +0530

    initial commit vid added

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ st.set_page_config(layout="wide")
 image = Image.open('nn.jpg')
 st.image(image, caption='it is my first web page deployment to cloud')
 st.write("Welcome to the page")
+video_file = open('vi.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
